701b5f7394a5d9176ae8d25aef4a7e55106e794f
Omer Dobzinsky <omer.d@walkme.com>
2020-06-21 14:13:42 +0300

    add more array operations

--- array.js
+++ array.js
@@ -7,6 +7,22 @@ const pop = ([x, ...xs]) => xs.length ? [x, ...pop(xs)] : [];
 exports.pop = pop;
 
 exports.unshift = (value, arr) => [value, ...arr];
-const shift = ([x, ...xs]) => xs;
+const shift = ([_, ...xs]) => xs;
 
 exports.shift = shift;
+
+const concat = (arr1, arr2) => [...arr1, ...arr2];
+exports.concat = concat;
+
+const reduce = (reducer, init, arr) => arr.reduce(reducer, init);
+exports.reduce = reduce;
+
+const map = (fn, arr) => arr.map(fn);
+exports.map = map;
+
+exports.filter = (fn, arr) => arr.filter(fn);
+
+const flat = arr => reduce(concat, [], arr);
+exports.flat = flat;
+
+exports.flatMap = (fn, arr) => flat(map(fn, arr));
--- array.spec.js
+++ array.spec.js
@@ -1,4 +1,17 @@
-const { head, tail, push, pop, unshift, shift } = require('./array');
+const {
+    head,
+    tail,
+    push,
+    pop,
+    unshift,
+    shift,
+    concat,
+    reduce,
+    map,
+    filter,
+    flat,
+    flatMap
+} = require('./array');
 
 describe('head', () => {
     test('should return first element of an array', () => {
@@ -24,7 +37,7 @@ describe('push', () => {
     test('should return an array with added element at the end', () => {
         const actual = push(3, [1, 2]);
 
-        expect(actual).toEqual(expect.arrayContaining([1, 2, 3]));
+        expect(actual).toEqual([1, 2, 3]);
     });
 
     test('should not change original array', () => {
@@ -32,7 +45,7 @@ describe('push', () => {
 
         push(3, actual);
 
-        expect(actual).toEqual(expect.arrayContaining([1, 2]));
+        expect(actual).toEqual([1, 2]);
     });
 });
 
@@ -40,7 +53,7 @@ describe('pop', () => {
     test('should return an array without last element', () => {
         const actual = pop([1, 2, 3]);
 
-        expect(actual).toEqual(expect.arrayContaining([1, 2]));
+        expect(actual).toEqual([1, 2]);
     });
 
     test('should not change original array', () => {
@@ -48,7 +61,7 @@ describe('pop', () => {
 
         pop(actual);
 
-        expect(actual).toEqual(expect.arrayContaining([1, 2, 3]));
+        expect(actual).toEqual([1, 2, 3]);
     });
 
     test('should return empty array fo array with only 1 member', () => {
@@ -62,7 +75,7 @@ describe('unshift', () => {
     test('should return an array with added element at the beginning', () => {
         const actual = unshift(1, [2, 3]);
 
-        expect(actual).toEqual(expect.arrayContaining([1, 2, 3]));
+        expect(actual).toEqual([1, 2, 3]);
     });
 
     test('should not change original array', () => {
@@ -70,7 +83,7 @@ describe('unshift', () => {
 
         unshift(3, actual);
 
-        expect(actual).toEqual(expect.arrayContaining([1, 2]));
+        expect(actual).toEqual([1, 2]);
     });
 });
 
@@ -86,12 +99,104 @@ describe('shift', () => {
 
         shift(actual);
 
-        expect(actual).toEqual(expect.arrayContaining([1, 2, 3]));
+        expect(actual).toEqual([1, 2, 3]);
     });
 
     test('should return empty array fo array with only 1 member', () => {
         const actual = shift([1]);
 
         expect(actual.length).toBe(0);
+    });
+});
+
+describe('concat', () => {
+    test('should return array containing both arrays', () => {
+        const actual = concat([1, 2], [3, 4]);
+
+        expect(actual).toEqual([1, 2, 3, 4]);
+    });
+
+    test('should not change original arrays', () => {
+        const arr1 = [1, 2];
+        const arr2 = [3, 4];
+
+        concat(arr1, arr2);
+
+        expect(arr1).toEqual([1, 2]);
+        expect(arr2).toEqual([3, 4]);
+    });
+});
+
+describe('reduce', () => {
+    test('should reduce an array', () => {
+        expect(reduce((x, y) => x + y, 0, [1, 2, 3])).toBe(6);
+    });
+
+    test('should return the initializer for an empty array', () => {
+        expect(reduce((x, y) => x + y, 0, [])).toBe(0);
+    });
+
+    test('should not change the original array', () => {
+        const actual = [1, 2, 3];
+
+        reduce((x, y) => x + y, 0, actual);
+
+        expect(actual).toEqual([1, 2, 3]);
+    });
+});
+
+describe('map', () => {
+    test('should return array with projected values', () => {
+        expect(map(x => x + 1, [1, 2, 3])).toEqual([2, 3, 4]);
+    });
+
+    test('should not change original array', () => {
+        const actual = [1, 2, 3];
+
+        map(x => x + 1, actual);
+
+        expect(actual).toEqual([1, 2, 3]);
+    });
+});
+
+describe('filter', () => {
+    test('should return array only with items matching predicate', () => {
+        expect(filter(x => x % 2 === 0, [1, 2, 3])).toEqual([2]);
+    });
+
+    test('should not change original array', () => {
+        const actual = [1, 2, 3];
+
+        filter(x => x % 2 === 0, actual);
+
+        expect(actual).toEqual([1, 2, 3]);
+    });
+});
+
+describe('flat', () => {
+    test('should flat an array 1 level', () => {
+        expect(flat([[1, 2], [3, 4]])).toEqual([1, 2, 3, 4]);
+    });
+
+    test('should not change original array', () => {
+        const actual = [[1], [2], [3]];
+
+        flat(actual);
+
+        expect(actual).toEqual([[1], [2], [3]]);
+    });
+});
+
+describe('flatMap', () => {
+    test('should return array of concated results', () => {
+        expect(flatMap(x => [x, x], [1, 2, 3])).toEqual([1, 1, 2, 2, 3, 3]);
+    });
+
+    test('should not change original array', () => {
+        const actual = [1, 2, 3];
+
+        flatMap(x => [x, x], actual);
+
+        expect(actual).toEqual([1, 2, 3]);
     });
 });
