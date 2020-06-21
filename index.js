c810d9245f2ae57b008538e6fe90973332950a7d
Omer Dobzinsky <omer.d@walkme.com>
2020-06-21 11:57:45 +0300

    implement apply using call

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
-exports.bind = (fn, thisArg, preArgs = []) => (...args) => fn.call(thisArg, ...preArgs, ...args);
+const bind = (fn, thisArg, preArgs = []) => (...args) => fn.call(thisArg, ...preArgs, ...args);
+
+exports.bind = bind
+exports.apply = (fn, thisArg, ...preArgs) => bind(fn, thisArg, preArgs);
--- index.spec.js
+++ index.spec.js
@@ -1,4 +1,4 @@
-const { bind } = require('./index');
+const { bind, apply } = require('./index');
 
 describe('bind', () => {
     test('shoud return a function', () => {
@@ -51,6 +51,62 @@ describe('bind', () => {
         const bound = bind(function () { return this.foo; }, { foo: 'foo' });
 
         const actual = bound();
+
+        expect(actual).toBe('foo');
+    });
+});
+
+describe('apply', () => {
+    test('shoud return a function', () => {
+        const actual = apply(jest.fn());
+
+        expect(actual).toBeInstanceOf(Function);
+    });
+
+    test('returned function should call original function', () => {
+        const fn = jest.fn();
+        const applied = apply(fn);
+
+        applied();
+
+        expect(fn).toHaveBeenCalled();
+    });
+
+    test('returned function should call original function with supplied arguments', () => {
+        const fn = jest.fn();
+        const args = [1, 2, 3];
+        const applied = apply(fn, null, ...args);
+
+        applied();
+
+        expect(fn).toHaveBeenCalledWith(...args);
+    });
+
+    test('returned function should pass additional arguments to original function', () => {
+        const fn = jest.fn();
+        const args = [1, 2, 3];
+        const applies = apply(fn);
+
+        applies(...args);
+
+        expect(fn).toHaveBeenCalledWith(...args);
+    });
+
+    test('returned function should call origianl passing supplied and additional arguments', () => {
+        const fn = jest.fn();
+        const suppliedArgs = [1, 2, 3];
+        const additionalArgs = [4, 5, 6];
+        const applied = apply(fn, null, ...suppliedArgs);
+
+        applied(...additionalArgs);
+
+        expect(fn).toHaveBeenCalledWith(...suppliedArgs, ...additionalArgs);
+    });
+
+    test('returned function shold call original with given context', () => {
+        const applied = apply(function () { return this.foo; }, { foo: 'foo' });
+
+        const actual = applied();
 
         expect(actual).toBe('foo');
     });
